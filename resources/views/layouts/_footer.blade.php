1c5d6370dd9c01527f55f0e7c6b09df558bb115f
mekauri <121773570+mekauri@users.noreply.github.com>
2024-09-26 12:37:03 -0600

    Revert "Fixed Footer error"
    This reverts commit ec68caac576468548085a1d16a1b3097e6efefc3.

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <div class="footer navbar-footer">
     <!-- Centered Navbar and Copyright -->
     <nav class="footer navbar-expand-md navbar-light">
@@ -18,7 +19,22 @@
 <div class="copyright text-center mt-2">
     &copy; {{ config('lorekeeper.settings.site_name', 'Lorekeeper') }} v{{ config('lorekeeper.settings.version') }} {{ Carbon\Carbon::now()->year }}
 </div>
+=======
+<nav class="navbar navbar-expand-md navbar-light">
+    <ul class="navbar-nav ml-auto mr-auto">
+        <li class="nav-item"><a href="{{ url('info/about') }}" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="{{ url('info/terms') }}" class="nav-link">Terms</a></li>
+        <li class="nav-item"><a href="{{ url('info/privacy') }}" class="nav-link">Privacy</a></li>
+        <li class="nav-item"><a href="{{ url('reports/bug-reports') }}" class="nav-link">Bug Reports</a></li>
+        <li class="nav-item"><a href="mailto:{{ env('CONTACT_ADDRESS') }}" class="nav-link">Contact</a></li>
+        <li class="nav-item"><a href="http://deviantart.com/{{ env('DEVIANTART_ACCOUNT') }}" class="nav-link">deviantART</a></li>
+        <li class="nav-item"><a href="https://github.com/corowne/lorekeeper" class="nav-link">Lorekeeper</a></li>
+        <li class="nav-item"><a href="{{ url('credits') }}" class="nav-link">Credits</a></li>
+    </ul>
+</nav>
+<div class="copyright">&copy; {{ config('lorekeeper.settings.site_name', 'Lorekeeper') }} v{{ config('lorekeeper.settings.version') }} {{ Carbon\Carbon::now()->year }}</div>
 
 @if(Config::get('lorekeeper.extensions.scroll_to_top'))
     @include('widgets/_scroll_to_top')
 @endif
+>>>>>>> bb2553513f851cc4addcc9b7b1536a2182fbe158
